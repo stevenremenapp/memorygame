8c5305a18df3b59b4061db48d25f2072bebe4eaf
stevenremenapp <remenapp@gmail.com>
2018-06-25 20:25:44 -0400

    Minor style changes; Add beginning modal; Add partially functioning restart button

--- js/app.js
+++ js/app.js
@@ -1,51 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
- // Shuffle function from http://stackoverflow.com/a/2450976
-// function shuffle(array) {
-//  let currentIndex = array.length, temporaryValue, randomIndex;
-//
-//  while (currentIndex !== 0) {
-//    randomIndex = Math.floor(Math.random() * currentIndex);
-//    currentIndex--;
-//    temporaryValue = array[currentIndex];
-//    array[currentIndex] = array[randomIndex];
-//    array[randomIndex] = temporaryValue;
-//  }
-//  return array;
-// }
-
- /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-// let cardDeck = document.querySelector('.deck');
-//
-//
-// cardDeck.addEventListener('click', function(event) {
-//   const clickedCard = event.target;
-//   console.log(clickedCard);
-//   if (clickedCard.classList.contains('card')) {
-//     clickedCard.classList.toggle('open');
-//     clickedCard.classList.toggle('show');
-//   }
-// });
-
 const cards = [{
   'name': 'typingCat',
   'src': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy-downsized.gif'
@@ -80,44 +32,6 @@ const cards = [{
 }
 ];
 
-// Shuffle the cards
-let gameGrid = cards.concat(cards);
-gameGrid.sort(function() {
-  return 0.5 - Math.random()
-});
-
-// Create cards grid and append to the DOM
-const game = document.getElementById('game');
-const grid = document.createElement('section');
-grid.setAttribute('class', 'grid');
-game.appendChild(grid);
-
-// Display the cards on the screen
-// Steve Griffith: https://www.youtube.com/watch?v=AqgVLYpBWG8
-for (let i = 0; i < gameGrid.length; i++) {
-  console.log(gameGrid[i]);
-  console.log(gameGrid[i].name);
-  console.log(gameGrid[i].src);
-  const card = document.createElement('div');
-  card.classList.add('card');
-  card.dataset.name = gameGrid[i].name;
-
-  // Create front of card
-
-  const front = document.createElement('div');
-  front.classList.add('front');
-
-  // Create back of card
-  const back = document.createElement('div');
-  back.classList.add('back');
-  back.style.backgroundImage = `url(${gameGrid[i].src})`;
-
-  // Append to grid and card
-  grid.appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
-}
-
 // Variables to track guessing in gameplay
 // Restriction to selecting only two cards only works if selectedCount is outside of the event listener function
 let selectedCount = 0;
@@ -127,49 +41,105 @@ let secondGuess = '';
 let delay = 1000;
 let moveCount = 0;
 
-// Allow to select certain cards
-grid.addEventListener('click', function(event) {
-  // Grab the clicked item
-  let clickedCard = event.target;
 
-  // Avoid clicking on certain items
-  if (clickedCard.nodeName === 'SECTION' || clickedCard.parentNode.classList.contains('selected') || clickedCard.parentNode.classList.contains('matched')) {
-      return;
+//Function to start gameplay
+function startGame() {
+  //Close the modal
+  closeModal();
+
+  //Check for existing game and remove it
+  checkForGrid();
+
+  // Shuffle the cards
+  let gameGrid = cards.concat(cards);
+  gameGrid.sort(function() {
+    return 0.5 - Math.random()
+  });
+
+  // Create cards grid and append to the DOM
+  const game = document.getElementById('game');
+  const grid = document.createElement('section');
+  grid.setAttribute('class', 'grid');
+  game.appendChild(grid);
+
+  // Display the cards on the screen
+  // Steve Griffith: https://www.youtube.com/watch?v=AqgVLYpBWG8
+  for (let i = 0; i < gameGrid.length; i++) {
+    console.log(gameGrid[i]);
+    console.log(gameGrid[i].name);
+    console.log(gameGrid[i].src);
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.name = gameGrid[i].name;
+
+    // Create front of card
+
+    const front = document.createElement('div');
+    front.classList.add('front');
+
+    // Create back of card
+    const back = document.createElement('div');
+    back.classList.add('back');
+    back.style.backgroundImage = `url(${gameGrid[i].src})`;
+
+    // Append to grid and card
+    grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
   }
 
-  // Only allow two selected cards
-  if (selectedCount < 2) {
-    selectedCount++;
-    // Track which card was selected for first guess
-    if (selectedCount === 1) {
-      firstGuess = clickedCard.parentNode.dataset.name;
-      clickedCard.parentNode.classList.add('selected');
-      // Track which card was selected for second guess
-    } else {
-      secondGuess = clickedCard.parentNode.dataset.name;
-      clickedCard.parentNode.classList.add('selected');
+  //Start the timer
+  timer();
+
+  //Initiate gameplay function
+  gameplay();
+}
+
+// Gameplay wrapped up in function
+function gameplay() {
+  let grid = document.querySelector('.grid');
+  // Allow to select certain cards
+  grid.addEventListener('click', function(event) {
+    // Grab the clicked item
+    let clickedCard = event.target;
+
+    // Avoid clicking on certain items
+    if (clickedCard.nodeName === 'SECTION' || clickedCard.parentNode.classList.contains('selected') || clickedCard.parentNode.classList.contains('matched')) {
+        return;
     }
-    // Run the match function if both guesses are not empty and the guesses' dataset names match
-    if (firstGuess !== '' && secondGuess !== '') {
-      if (firstGuess === secondGuess) {
-        setTimeout(selectedCardsMatch, delay);
-        setTimeout(resetGuesses, delay);
-        setTimeout(moveCounter, delay);
-        setTimeout(displayCatRating, delay);
+
+    // Only allow two selected cards
+    if (selectedCount < 2) {
+      selectedCount++;
+      // Track which card was selected for first guess
+      if (selectedCount === 1) {
+        firstGuess = clickedCard.parentNode.dataset.name;
+        clickedCard.parentNode.classList.add('selected');
+        // Track which card was selected for second guess
       } else {
-        setTimeout(resetGuesses, delay);
-        setTimeout(moveCounter, delay);
-        setTimeout(displayCatRating, delay);
+        secondGuess = clickedCard.parentNode.dataset.name;
+        clickedCard.parentNode.classList.add('selected');
       }
+      // Run the match function if both guesses are not empty and the guesses' dataset names match
+      if (firstGuess !== '' && secondGuess !== '') {
+        if (firstGuess === secondGuess) {
+          setTimeout(selectedCardsMatch, delay);
+          setTimeout(resetGuesses, delay);
+          setTimeout(moveCounter, delay);
+          setTimeout(displayGameRating, delay);
+        } else {
+          setTimeout(resetGuesses, delay);
+          setTimeout(moveCounter, delay);
+          setTimeout(displayGameRating, delay);
+        }
+      }
+      //Did not work with previousSelection assigned with let
+      // previousSelection = clickedCard;
+      // console.log(previousSelection);
+      // console.log(clickedCard);
     }
-    //Did not work with previousSelection assigned with let
-    // previousSelection = clickedCard;
-    // console.log(previousSelection);
-    // console.log(clickedCard);
-  }
-});
-
-timer();
+  });
+}
 
 // Function to check if cards match
 function selectedCardsMatch() {
@@ -211,16 +181,11 @@ function moveCounter() {
 }
 
 // Function to track and display cat rating
-function displayCatRating() {
+function displayGameRating() {
   let catsList = document.getElementById('catRating');
   let cat = document.querySelectorAll('.cat');
-  if (moveCount === 12) {
-    catsList.removeChild(cat[0]);
-  } else if (moveCount === 17) {
-    catsList.removeChild(cat[0]);
-  } else if (moveCount === 22) {
-    catsList.removeChild(cat[0]);
-  } else if (moveCount === 27) {
+  let ratingArray = [2, 4, 6, 8];
+  if (ratingArray.indexOf(moveCount) > -1) {
     catsList.removeChild(cat[0]);
   }
 }
@@ -243,12 +208,59 @@ function timer() {
     document.querySelector('.minutes').innerHTML = padValue(parseInt(sec/60,10));
     // let secs = document.querySelector('.seconds').textContent;
     // console.log(secs);
-    let mins = document.querySelector('.minutes').textContent;
-    console.log(mins);
+    // let mins = document.querySelector('.minutes').textContent;
+    // console.log(mins);
 
     // RUN FUNCTION THAT CHANGES SCREEN IF MORE THAN 30 MINUTES HAS ELAPSED
 
   }, 1000);
-
-
 }
+
+//Restart Icon functionality
+let restartBtn = document.getElementById('restart');
+restartBtn.addEventListener('click', startGame);
+
+function checkForGrid() {
+  let grid = document.querySelector('.grid');
+  let test = document.body.contains(game);
+  console.log(test);
+  if (document.body.contains(grid)) {
+    grid.parentNode.removeChild(grid);
+  }
+}
+
+
+
+// Modal Functioning
+
+//Grab elements
+let startGameModal = document.getElementById('startGameModal');
+// let closeBtn = document.getElementsByClassName('closeBtn')[0];
+let startGameBtn = document.getElementById('startGame');
+
+//Listen for open click
+// modalBtn.addEventListener('click', openModal);
+//Listen for close click
+// closeBtn.addEventListener('click', closeModal);
+//Listen for closing click outside of dialog window
+// window.addEventListener('click', clickOutside);
+
+//Listen for click on the Go! button
+startGameBtn.addEventListener('click', startGame);
+
+//Funtion to open modal
+// function openModal() {
+//   modal.style.display = 'block';
+// };
+
+//Function to close modal
+function closeModal() {
+  startGameModal.style.display = 'none';
+};
+
+//Function to close modal if user clicks outside of dialog
+// function clickOutside(event) {
+//   if (event.target == startGameModal) {
+//   modal.style.display = 'none';
+//   }
+// }
